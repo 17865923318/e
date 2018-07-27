feae0dee6a2398442354c78da71b8f4ef7a78773
Administrator <Administrator@ZX-PC>
2018-07-27 17:19:48 +0800

    修改了hello文件

--- src/main/java/e/Hello.java
+++ src/main/java/e/Hello.java
@@ -1,5 +1,16 @@
 package e;
 
 public class Hello {
+	
+	private String Studentname;
 
+	public String getStudentname() {
+		return Studentname;
+	}
+
+	public void setStudentname(String studentname) {
+		Studentname = studentname;
+	}
+	
+	
 }
